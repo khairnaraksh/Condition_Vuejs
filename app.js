327b2e01fc897047b2d1944f53d61b616e76ff1b
Akshay Khairnar <khairnaraksh@gmail.com>
2017-08-02 21:20:43 +0530

    Conditional Statements

---
+++ app.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#vue-app',
+    data: {
+    name: 'Aksh',
+    characters: ['Mamta','Aksh','Sid','Gauri'],
+    persons: [
+      {name:'Mamta',age:25},
+      {name:'Mansi',age:24},
+      {name:'Sakshi',age:20}
+    ]
+    },
+  methods: {
+
+  },
+  computed:{
+
+  }
+});
